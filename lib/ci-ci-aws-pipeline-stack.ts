1be750a94cf8e3e030b963af09cd0ec24da4d841
baixiang.liu <baixiang.liu@evalueserve.com>
2023-03-01 19:28:48 -0800

    This is my first commit

--- lib/ci-ci-aws-pipeline-stack.ts
+++ lib/ci-ci-aws-pipeline-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+// import { MyPipelineAppStage } from './stage';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CiCiAwsPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CiCiAwsPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth',{
+        input: CodePipelineSource.gitHub('gitsdd', 'main'),
+        commands: [ 'npm ci',
+                    'npm run build',
+                    'npm ckd synth']
+      }),
+    });
   }
 }
